ad6187e527a41360452ec1116189f0f40a4f023a
DESKTOP-2LVKBQD\user <mayhy28@chunjae.co.kr>
2022-10-31 10:53:13 +0900

    commit

--- js/script.js
+++ js/script.js
@@ -20,35 +20,33 @@ $(document).ready(function(){
         console.log(idx_01);
         idx_01--;
         if (idx_01 < 0) {
-            idx_01 = 0
-        }
-        viewItem(idx_01);
-        Indigate(idx_01);
-    });
-  
-    $('.next a').on('click', function(e){
-        e.preventDefault();
-        idx_01++;
-        if (idx_01 > $(".tab_btns li").length - 1) {
+            // idx_01 = 0
             idx_01 = $(".tab_btns li").length - 1
         }
         viewItem(idx_01);
         Indigate(idx_01);
     });
-
+    $('.next a').on('click', function(e){
+        e.preventDefault();
+        idx_01++;
+        console.log(idx_01);
+        if (idx_01 > $(".tab_btns li").length - 1) {
+            // idx_01 = $(".tab_btns li").length - 1
+            idx_01 = 0
+        }
+        viewItem(idx_01);
+        Indigate(idx_01);
+    });
     function viewItem(idx_01){
-        
         var idx_01 = idx_01;
         $('.tab_area li').hide();
         $('.tab_area li').eq(idx_01).show();
     }
     function Indigate(idx_01){
-
         var idx_01 = idx_01;
         $('.tab_btns li').removeClass('active');
 	    $('.tab_btns li').eq(idx_01).addClass('active');
     }
-
 
     // 팝업창
     $('.img_box').on('click', function(e){
