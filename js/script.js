6018b2c217b2b728a87faffc1db62224917d1d29
DESKTOP-2LVKBQD\user <mayhy28@chunjae.co.kr>
2022-11-01 16:01:25 +0900

    commit

--- js/script.js
+++ js/script.js
@@ -15,12 +15,33 @@ $(document).ready(function(){
     });
 
     // 탭메뉴 좌/우 이동
+    // $('.prev a').on('click', function(e){
+    //     e.preventDefault();
+    //     console.log(idx_01);
+    //     idx_01--;
+    //     if (idx_01 < 0) {
+    //         idx_01 = 0
+    //         // idx_01 = $(".tab_btns li").length - 1
+    //     viewItem(idx_01);
+    //     Indigate(idx_01);
+    // });
+    // $('.next a').on('click', function(e){
+    //     e.preventDefault();
+    //     idx_01++;
+    //     console.log(idx_01);
+    //     if (idx_01 > $(".tab_btns li").length - 1) {
+    //         idx_01 = $(".tab_btns li").length - 1
+    //     }
+    //     viewItem(idx_01);
+    //     Indigate(idx_01);
+    // });
+
+    // 탭메뉴 루프
     $('.prev a').on('click', function(e){
         e.preventDefault();
         console.log(idx_01);
         idx_01--;
         if (idx_01 < 0) {
-            // idx_01 = 0
             idx_01 = $(".tab_btns li").length - 1
         }
         viewItem(idx_01);
@@ -31,12 +52,12 @@ $(document).ready(function(){
         idx_01++;
         console.log(idx_01);
         if (idx_01 > $(".tab_btns li").length - 1) {
-            // idx_01 = $(".tab_btns li").length - 1
             idx_01 = 0
         }
         viewItem(idx_01);
         Indigate(idx_01);
     });
+    
     function viewItem(idx_01){
         var idx_01 = idx_01;
         $('.tab_area li').hide();
