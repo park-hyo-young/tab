883c95e161200469f715a2ca2b1604026547753f
DESKTOP-2LVKBQD\user <mayhy28@chunjae.co.kr>
2022-11-02 13:15:10 +0900

    commit

--- js/script.js
+++ js/script.js
@@ -4,26 +4,17 @@ $(document).ready(function(){
     // 탭 메뉴 전환
     $('.tab_btns').find("li").on('click', function(e){
         e.preventDefault();
-        var me = $(e.currentTarget);
-            idx_01 = me.index();
+        var This = $(e.currentTarget);
+            idx_01 = This.index();
 
+        console.log(idx_01);
         $('.tab_btns li').removeClass('active');
-        me.addClass('active');
+        This.addClass('active');
         
         viewItem(idx_01);
     });
 
     // 탭메뉴 좌/우 이동
-    // $('.prev a').on('click', function(e){
-    //     e.preventDefault();
-    //     idx_01--;
-    //     if (idx_01 < 0) {
-    //         idx_01 = 0
-    //     }
-    //      console.log(idx_01);
-    //     viewItem(idx_01);
-    //     Indigate(idx_01);
-    // });
     // $('.next a').on('click', function(e){
     //     e.preventDefault();
     //     idx_01++;
@@ -31,21 +22,21 @@ $(document).ready(function(){
     //         idx_01 = $(".tab_btns li").length - 1
     //     }
     //      console.log(idx_01);
-    //     viewItem(idx_01);
     //     Indigate(idx_01);
+    //     viewItem(idx_01);
+    // });
+    // $('.prev a').on('click', function(e){
+    //     e.preventDefault();
+    //     idx_01--;
+    //     if (idx_01 < 0) {
+    //         idx_01 = 0
+    //     }
+    //      console.log(idx_01);
+    //     Indigate(idx_01);
+    //     viewItem(idx_01);
     // });
 
     // 탭메뉴 루프
-    $('.prev a').on('click', function(e){
-        e.preventDefault();
-        idx_01--;
-        if (idx_01 < 0) {
-            idx_01 = $(".tab_btns li").length - 1;
-        }
-        console.log(idx_01);
-        viewItem(idx_01);
-        Indigate(idx_01);
-    });
     $('.next a').on('click', function(e){
         e.preventDefault();
         idx_01++;
@@ -53,8 +44,18 @@ $(document).ready(function(){
             idx_01 = 0;
         }
         console.log(idx_01);
-        viewItem(idx_01);
         Indigate(idx_01);
+        viewItem(idx_01);
+    });
+    $('.prev a').on('click', function(e){
+        e.preventDefault();
+        idx_01--;
+        if (idx_01 < 0) {
+            idx_01 = $(".tab_btns li").length - 1;
+        }
+        console.log(idx_01);
+        Indigate(idx_01);
+        viewItem(idx_01);
     });
     
     function viewItem(idx_01){
